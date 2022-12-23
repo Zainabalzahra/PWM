f7724b15e40d665f94c4e9a9bdf8eed470f3593b
Zainabalzahra <99761119+Zainabalzahra@users.noreply.github.com>
2022-12-23 14:26:59 +0600

    Update PWM.ino

--- PWM.ino
+++ PWM.ino
@@ -2,13 +2,29 @@ int d = 5;
 void setup() {
   // put your setup code here, to run once:
 pinMode(3, OUTPUT);
-
-
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(11, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- for (int a =0; a < 256; a++)
+ 
+   for (int a =0; a < 256; a++)
+ {
+  analogWrite(11, a);
+  delay(d);
+ }
+ 
+ for (int a =255; a>=0; a--)
+ {
+  analogWrite(11, a);
+  delay(d);
+ }
+ delay(200);
+  
+  for (int a =0; a < 256; a++)
  {
   analogWrite(3, a);
   delay(d);
@@ -21,6 +37,20 @@ void loop() {
  }
  delay(200);
 
+   for (int a =0; a < 256; a++)
+ {
+  analogWrite(6, a);
+  delay(d);
+ }
+ 
+ for (int a =255; a>=0; a--)
+ {
+  analogWrite(6, a);
+  delay(d);
+ }
+ delay(200);
+
+  
  for (int a =0; a < 256; a++)
  {
   analogWrite(5, a);
@@ -30,6 +60,19 @@ void loop() {
  for (int a =255; a>=0; a--)
  {
   analogWrite(5,  a);
+  delay(d);
+ }
+ delay(200);
+  
+  for (int a =0; a < 256; a++)
+ {
+  analogWrite(9, a);
+  delay(d);
+ }
+ 
+ for (int a =255; a>=0; a--)
+ {
+  analogWrite(9,  a);
   delay(d);
  }
  delay(200);
